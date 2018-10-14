a28d8b0b8c9ba97b35d3fed5f98229d12564c4c3
ccasil <cesarkylecasil@gmail.com>
2018-10-14 13:33:32 -0700

    navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,46 +2,26 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 
 class Navbar extends Component {
-
-    state = {
-        selected: 'home',
-        expanded: false
-    };
-
-    onSelect = (selected) => {
-        this.setState({ selected: selected });
-    };
-    onToggle = (expanded) => {
-        this.setState({ expanded: expanded });
-    };
-
-    navigate = (pathname) => () => {
-        this.setState({ selected: pathname });
-    };
-
-
     render() {
-
-        const { expanded, selected } = this.state;
-
         return (
             <div>
+                {/* <h1 className="display-1">Megumi Hiramoto</h1> */}
 
-                <h1 className="display-1">Megumi Hiramoto</h1>
-
-                
-                {/* <nav class="navbar navbar-light">
+                <nav class="navbar fixed-top navbar-light text-center">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
+                    <a class="navbar-brand text-center name" href="#">Megumi Hiramoto</a>
                     <div class="collapse navbar-collapse" id="navbarsExample01">
                         <ul class="navbar-nav mr-auto float-left">
+                            <li class="nav-item active">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
                             <li class="nav-item active">
                                 <Link className="nav-link" to="/magazine">Magazine</Link>
                             </li>
@@ -62,69 +42,8 @@ class Navbar extends Component {
                             </li>
                         </ul>
                     </div>
-                </nav> */}
+                </nav>
 
-
-                <SideNav style={{ background: 'black', position: 'fixed' }} onSelect={this.onSelect} onToggle={this.onToggle}>
-                    <SideNav.Toggle />
-                    <SideNav.Nav selected={selected}>
-                        <NavItem eventKey="Home">
-                            <NavIcon>
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Home">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/"> Home </Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="Magazine">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/magazine"><i className="fas fa-book-open" style={{ fontSize: '1.75em', verticalAlign: 'middle' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Magazine">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/magazine"> Magazine </Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="Blog">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/blog"><i className="fas fa-sign-in-alt" style={{ fontSize: '1.75em', verticalAlign: 'middle' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Blog">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/blog"> Blog </Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="Styling">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/styling"><i className="fas fa-user-tie" style={{ fontSize: '1.75em' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Styling">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/styling">Styling</Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="Documentary">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/documentary"><i className="fas fa-tv" style={{ fontSize: '1.75em' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Documentary">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/documentary"> Documentary </Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="About">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/about"><i className="fas fa-female" style={{ fontSize: '1.75em' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="About">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/about"> About </Link>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="Resume">
-                            <NavIcon>
-                                {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/resume"><i className="fas fa-file-alt" style={{ fontSize: '1.75em' }} /></Link> */}
-                            </NavIcon>
-                            <NavText style={{ fontSize: '1.75em' }} title="Resume">
-                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/resume"> Resume </Link>
-                            </NavText>
-                        </NavItem>
-                    </SideNav.Nav>
-                </SideNav>
             </div>
         );
     }
